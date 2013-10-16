a49ab100e73e2c484da7e4b7d16175e3cd24e2bf
carlssonn <pontus_carlsson@hotmail.com>
2013-10-16 10:41:24 +0200

    first!

---
+++ Lab5/src/model/Memory.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.*;
+
+public class Memory {
+	
+	- Users : ArrayList<User>
+	- cards : ArrayList<Card>
+	- time : Timer
+	- turns : int
+	- score : int
+	public Memory() {
+		reset();
+	}
+	
+	public void reset() {
+	//Reset everything but the score
+	}
+	
+	public void turnCard() {
+		
+	}
+
+	public boolean checkIfPair() {
+		return false;
+	}
+	
+	public boolean checkIfDone() {
+		return false;
+	}
+	
+	public void saveUserInfo() {
+		
+	}
+
+
+}
